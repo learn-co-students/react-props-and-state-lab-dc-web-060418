9f639491973e15f337e28ddbdab596039fad03b9
wley3337 <wley3337@gmail.com>
2018-08-07 15:17:23 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,59 @@ class App extends React.Component {
     }
   }
 
+  onAdoptPet = (petId) =>{
+   const newPets = this.state.pets;
+    for(const pet of newPets){
+      if(pet.id === petId){
+        console.log(pet.isAdopted)
+        pet.isAdopted = pet.isAdopted === true ?  false : true
+        console.log(pet.isAdopted)
+      }
+    }
+   
+    this.setState({
+      pets: newPets
+    })
+    
+  }
+  
+
+  onChangeType(filterType){
+    this.setState({
+      filters: {
+        type: filterType,
+      }
+    })
+  }
+
+
+  onFindPetsClick= () => {
+  
+    let extension;
+    switch(this.state.filters.type){
+      case 'all':
+      extension = '';
+      break;
+      case 'cat':
+      extension = '?type=cat';
+      break;
+      case 'dog':
+      extension = '?type=dog';
+      break;
+      case 'micropig':
+      extension = '?type=micropig';
+      break;
+    }
+
+    fetch(`/api/pets`+ extension)
+    .then(response => response.json())
+    .then(results=>{
+      this.setState({
+      pets: results
+    })
+  });
+}
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +77,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType.bind(this)}  onFindPetsClick={this.onFindPetsClick.bind(this)}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
